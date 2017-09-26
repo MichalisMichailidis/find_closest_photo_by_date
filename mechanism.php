f818c4eb105d0512c5957b93d785264a18f4b4c1
Pericles Bogris <Periklisbogris@gmail.com>
2017-09-26 17:10:33 +0300

    mechanism.php
    Updated mechanism

--- mechanism.php
+++ mechanism.php
@@ -1,71 +1,65 @@
-<?php 
-// Array with file names in file directory games
-$photos_array                     = array();
+        <?php // Array with file names in file directory games
 
- //Creates an array with name of photos which we need  
-$photos_array_with_same_or_next_datename = array();
-foreach (glob('./games/*.*') as $filename) {
-    $filename = str_replace("./games/", "", $filename);
-    $filename = str_replace(".jpg", "", $filename);
-    array_push($photos_array, $filename);
-}
-for ($i = 0; $i < count($photos_array); $i++) {
-    //echo $photos_array[$i] . "<br />";
-    $date_of_photo                 = $photos_array[$i];
-    $date_of_photo_whithout_dashes = str_replace("-", "", $date_of_photo);
-    //Finds the day of photos as string
-    $day_of_photo                  = substr($date_of_photo_whithout_dashes, 0, 2);
-    //Converts day from string to integer 
-    $day_of_photo_int              = intval($day_of_photo);
-    
-    //Finds the month of photos as string
-    $month_of_photo                       = substr($date_of_photo_whithout_dashes, 2, 2);
-    //Converts day from string to integer 
-    $month_of_photo_int                   = intval($month_of_photo);
-    //Current Date
-    $current_date                         = date("d/m/Y");
-    $formated_current_date_without_dashes = str_replace("/", "", $current_date);
-    
-    //Finds the day as string from Current day
-    $current_day      = substr($formated_current_date_without_dashes, 0, 2);
-    //Converts current day from string to integer 
-    $current_date_int = intval($current_day);
-    
-    //Finds the month as string from Current day
-    $current_mothn     = substr($formated_current_date_without_dashes, 2, 2);
-    //Converts current month from string to integer 
-    $current_mothn_int = intval($current_mothn);
-    
-    
-    
-    //Checks if currnet month is greater than each photos month
-    if ($current_mothn_int <= $month_of_photo_int) {
-        if ($current_date_int <= $day_of_photo_int) {
-            
+        $photos_array = array();
+        $photos_array_with_older_datename=array();
+        foreach(glob('.***image path*** *.*') as $filename){
+            $filename = str_replace(".***image path***", "", $filename);
+            $filename = str_replace(".jpg", "", $filename);
+            array_push($photos_array,$filename);
+         }
+        for($i=0; $i < count($photos_array); $i++){
+              //echo $photos_array[$i] . "<br />";
+                $date_of_photo= $photos_array[$i];
+                $date_of_photo_whithout_dashes = str_replace("-", "", $date_of_photo);
+                //Finds the day of photos as string
+                $day_of_photo =  substr($date_of_photo_whithout_dashes, 0, 2);
+                //Converts day from string to integer 
+                $day_of_photo_int = intval($day_of_photo);
+             
+                //Finds the month of photos as string
+                $month_of_photo = substr($date_of_photo_whithout_dashes, 2, 2);
+                //Converts day from string to integer 
+                $month_of_photo_int = intval($month_of_photo);
+                //Current Date
+                $current_date = date("d/m/Y");
+                $formated_current_date_without_dashes = str_replace("/", "", $current_date);
+                
+                //Finds the day as string from Current day
+                $current_day = substr($formated_current_date_without_dashes, 0, 2);
+                //Converts current day from string to integer 
+                $current_date_int = intval($current_day);
+                 
+                //Finds the month as string from Current day
+                $current_mothn = substr($formated_current_date_without_dashes, 2, 2); 
+                //Converts current month from string to integer 
+                $current_mothn_int = intval($current_mothn);  
 
-            array_push($photos_array_with_same_or_next_datename, $photos_array[$i]);
-            
+                         
+                
+                 //Checks if currnet month is greater than each photos month
+                 if ( $current_mothn_int <= $month_of_photo_int ){
+                       // if($current_date_int <= $day_of_photo_int){
+                            
+                            //Creates an array with name of photos which passes the if statment from above  
+                            
+                            array_push( $photos_array_with_older_datename,$photos_array[$i]);
+                                
+                              
+                        // }
+                 }
+
+
             
         }
-    }
-    
-    
-    
-}
 
-// Number of photos in $photos_array_with_same_or_next_datename array
-$number_of_photos_with_older_datename_in_array = count($photos_array_with_same_or_next_datename);
+        $number_of_photos_with_older_datename_in_array = count($photos_array_with_older_datename);
+        //Get the filename and print the photo
+        if($number_of_photos_with_older_datename_in_array == 0){
+               echo "<img class='next-game-photo' src='***image path***' />";
+        }else{
 
-//Check if the array contains at least one photo
-if ($number_of_photos_with_older_datename_in_array == 0) {
-    
-    // if there isn't any photo in array echo the default photo 
-    echo "<img class='***your class***' src='***Your Default Photo ***' />";
-} else {
-     
-     // echo the next available photo
-    echo "<img class='***your class***' src='/games/" . $photos_array_with_same_or_next_datename[0] . ".jpg' />";
-}
-;
+         echo "<img class='next-game-photo' src='***image path***" .  $photos_array_with_older_datename[0] . ".jpg' />";
+         };
 
-?>
+        ?>
+        <!-- END -->
